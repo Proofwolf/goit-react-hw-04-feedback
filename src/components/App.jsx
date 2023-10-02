44f5b6eb1055fb2a49e538df97d51ce361f91634
Proofwolf <65850243+Proofwolf@users.noreply.github.com>
2023-10-02 22:04:57 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+//Імпорт компонентів
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+//Імпорт відповідних іконок
+import { FcLike } from 'react-icons/fc';
+import { FcInfo } from 'react-icons/fc';
+import { FcDislike } from 'react-icons/fc';
+//Імпорт стилів
+import css from './App.module.css';
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const iconsButton = {
+    good: <FcLike size="30px" className={css.icon_good} />,
+    neutral: <FcInfo size="30px" />,
+    bad: <FcDislike size="30px" className={css.icon_bad} />,
+  };
+
+  //Функція-обробник кліка на відповідну кнопку
+  const onBtnClicked = option => {
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  //Функція підрахунку суми всіх фідбеків
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  //Функція підрахунку процентного співвідношення позитивних фідбеків
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const positivePercentage = Math.round((good / total) * 100);
+    return total ? positivePercentage : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const options = ['good', 'neutral', 'bad'];
+
+  return (
+    <div className={css.wrapper}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={onBtnClicked}
+          iconsButton={iconsButton}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
